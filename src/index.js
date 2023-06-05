198765cba9433d8b5bd81d761280629d1ce9bc80
inon farhan <inon.f4@gmail.com>
2023-06-05 15:11:12 +0300

    delete login with google

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <GoogleOAuthProvider clientId="180598139259-d65lhh7gr2ja5462gjviqfhdnk56aje5.apps.googleusercontent.com">
   <Provider store={store}>
     <App />
   </Provider>
-  // </GoogleOAuthProvider>
 );
 reportWebVitals();
